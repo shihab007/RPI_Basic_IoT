bbc2fae94ba5aa28a52da0429a1a28d11a8611d4
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-09-29 13:23:26 +0300

    Add color change option

--- fserver.py
+++ fserver.py
@@ -9,42 +9,97 @@ GPIO.setup(ledG, GPIO.OUT)
 GPIO.setup(ledR, GPIO.OUT)
 GPIO.setup(ledB, GPIO.OUT)
 
-GPIO.output(ledG, True)
-GPIO.output(ledR, True)
-GPIO.output(ledB, True)
+def cleanLed():
+    GPIO.output(ledG, True)
+    GPIO.output(ledR, True)
+    GPIO.output(ledB, True)
+
+def redLed():
+    GPIO.output(ledR, False)
+
+def blueLed():
+    GPIO.output(ledB, True)
+
+def greenLed():
+    GPIO.output(ledG, True)
 
 # make_server is used to create this simple python webserver
 from wsgiref.simple_server import make_server
 
 # Function that is ran when a http request comes in
 def simple_app(env, start_response):
-    
+
     # set some http headers that are sent to the browser
     status = '200 OK'
-    headers = [('Content-type', 'text/html')] 
+    headers = [('Content-type', 'text/html')]
     start_response(status, headers)
 
     # What did the user ask for?
-    if env["PATH_INFO"] == "/on":
+    if env["PATH_INFO"] == "/r":
         print("user asked for /on")
-	GPIO.output(ledB, False)
-	GPIO.output(ledG, False)
-	GPIO.output(ledR, False)
-	return "<html><body>got on<p><a href=\"/off\">Turn off</a></body></html>"
-	
-    elif env["PATH_INFO"] == "/off":
+        cleanLed()
+        redLed()
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="Red" href="/r">Red</a>'\
+                        '<a title="Green" href="/g">Green</a>'\
+                        '<a title="Blue" href="/b">Blue</a>'\
+                    '</body>'\
+                '</html>'
+
+    elif env["PATH_INFO"] == "/g":
         print("user asked for /off")
-	GPIO.output(ledB, True)
-	GPIO.output(ledR, True)
-	GPIO.output(ledG, True)
-	return "<html><body>got off <p><a href=\"/on\">Turn me on</a></body></html>"
+        cleanLed()
+        greenLed()
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="Red" href="/r">Red</a>'\
+                        '<a title="Green" href="/g">Green</a>'\
+                        '<a title="Blue" href="/b">Blue</a>'\
+                    '</body>'\
+                '</html>'
+
+    elif env["PATH_INFO"] == "/b":
+        print("user asked for /off")
+        cleanLed()
+        blueLed()
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="Red" href="/r">Red</a>'\
+                        '<a title="Green" href="/g">Green</a>'\
+                        '<a title="Blue" href="/b">Blue</a>'\
+                    '</body>'\
+                '</html>'
+
     else:
         print("user asked for something else")
-	return "<html><body><a href=\"/on\">Turn me on</a></body></html>"            
+        return '<!DOCTYPE html>'\
+                '<html>'\
+                    '<head>'\
+                        '<title>"Lights"</title>'\
+                    '</head>'\
+                    '<body>'\
+                        '<a title="Red" href="/r">Red</a>'\
+                        '<a title="Green" href="/g">Green</a>'\
+                        '<a title="Blue" href="/b">Blue</a>'\
+                    '</body>'\
+                '</html>'
 
 # Create a small python server
 httpd = make_server("", 8011, simple_app)
 print "Serving on port 8011..."
 print "You can open this in the browser http://192.168.1.xxx:8011 where xxx is your rpi ip aadress"
-print "Or if you run this server on your own computer then http://localhost:8011" 
+print "Or if you run this server on your own computer then http://localhost:8011"
 httpd.serve_forever()
